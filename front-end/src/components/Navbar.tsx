76207f7d3ce46a8efadce94c3696574ddfba8786
Nasir <nasir-islam@hotmail.com>
2022-12-25 00:03:25 +0000

    basic nav sidebar layout there

--- front-end/src/components/Navbar.tsx
+++ front-end/src/components/Navbar.tsx
@@ -1,11 +1,42 @@
-import React from 'react'
+import React from 'react';
 
-type Props = {}
+type Props = {};
 
 const Navbar = (props: Props) => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <div className="navbar-container bg-primary text-white flex flex-row p-3">
+      <a className="font-logo text-xl cursor-pointer hover:text-accent ">
+        TOWER <span className=" font-semibold">HAMLETS </span>
+        <span className="font-bold">MOSQUES</span>
+      </a>
 
-export default Navbar
+      <nav>
+        <button aria-controls="primary-navigation" className="hamburger" aria-expanded="false">
+          <span></span> Menu
+        </button>
+        <ul
+          id="primary-navigation"
+          className="primary-navigation text-primary font-semibold"
+        >
+          <li>
+            <a>Home</a>
+          </li>
+          <li>
+            <a>About Us</a>
+          </li>
+          <li>
+            <a>Jumu'ah Times</a>
+          </li>
+          <li>
+            <a>Contact Us</a>
+          </li>
+          <li>
+            <a>Support Us</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
--- front-end/tailwind.config.js
+++ front-end/tailwind.config.js
@@ -1,10 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],    // Pass in the paths to all the files using tailwind classes
+  content: ['./src/**/*.{js,jsx,ts,tsx}'], // Pass in the paths to all the files using tailwind classes
   theme: {
-    extend: {},
+    extend: {       // Add colors here so as not to reset defaults!!
+      colors: {
+        primary: '#0A1045',
+        accent: '#E9CD7A',
+      },
+      fontFamily: {
+        'logo': ['"Cormorant Garamond"', 'serif'],
+      },
+    },
   },
   plugins: [],
-}
+};
