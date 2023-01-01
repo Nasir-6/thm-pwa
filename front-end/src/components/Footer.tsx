9b87917367fc553d9651bd72679d072760025988
Nasir <nasir-islam@hotmail.com>
2023-01-01 01:14:02 +0000

    add links and hover effects to icons

--- front-end/src/components/Footer.tsx
+++ front-end/src/components/Footer.tsx
@@ -8,9 +8,9 @@ const Footer = (props: Props) => {
     <footer className='footer bg-primary text-white p-4 flex flex-col sm:flex-row gap-2 justify-center items-center'>
         <p className='copyright-text text-xs font-bold'>Copyright © 2023 Tower Hamlets Mosques, All rights reserved.</p>
         <div className='social-icons flex'>
-        <FaFacebook className='w-8'/>
-        <FaTwitter className='w-8'/>
-        <FaInstagram className='w-8' />
+        <a href='https://www.facebook.com/thmuslims'><FaFacebook className='w-8 hover:text-accent'/></a>
+        <a href='https://twitter.com/thmuslims'><FaTwitter className='w-8 hover:text-accent'/></a>
+        <a href='https://www.instagram.com/thmosques/'><FaInstagram className='w-8 hover:text-accent' /></a>
         </div>
     </footer>
   )
