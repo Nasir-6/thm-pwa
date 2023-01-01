d4684058a99453f3dbe0e113c176b94d3d9b1f2f
Nasir <nasir-islam@hotmail.com>
2023-01-01 00:39:22 +0000

    added social icons

--- front-end/src/components/Footer.tsx
+++ front-end/src/components/Footer.tsx
@@ -1,12 +1,16 @@
-import React from 'react'
+import React from 'react';
+import { FaFacebook, FaTwitter, FaInstagram } from 'react-icons/fa';
 
 type Props = {}
 
 const Footer = (props: Props) => {
   return (
-    <footer className='footer bg-primary w-100vw text-white p-3 flex '>
+    <footer className='footer bg-primary text-white p-3 flex '>
         <p className='copyright-text'>Copyright © 2023 Tower Hamlets Mosques, All rights reserved.</p>
-        
+        <FaFacebook className='w-8'/>
+        <FaTwitter className='w-8'/>
+        <FaInstagram className='w-8' />
+
     </footer>
   )
 }
