5cb346471ed7a15ef995c16f6f7be70c289c6c69
Nasir <nasir-islam@hotmail.com>
2023-01-01 01:04:32 +0000

    fix footer layout for mobile

--- front-end/src/components/Footer.tsx
+++ front-end/src/components/Footer.tsx
@@ -5,12 +5,13 @@ type Props = {}
 
 const Footer = (props: Props) => {
   return (
-    <footer className='footer bg-primary text-white p-3 flex '>
-        <p className='copyright-text'>Copyright © 2023 Tower Hamlets Mosques, All rights reserved.</p>
+    <footer className='footer bg-primary text-white p-4 flex flex-col sm:flex-row gap-2 justify-center items-center'>
+        <p className='copyright-text text-xs font-bold'>Copyright © 2023 Tower Hamlets Mosques, All rights reserved.</p>
+        <div className='social-icons flex'>
         <FaFacebook className='w-8'/>
         <FaTwitter className='w-8'/>
         <FaInstagram className='w-8' />
-
+        </div>
     </footer>
   )
 }
