4e84c80189ea3a12a4554b03d405fd1706497f7c
Nasir <nasir-islam@hotmail.com>
2023-01-02 01:21:40 +0000

    add working leaflet map with missing icons

--- front-end/src/pages/Home.tsx
+++ front-end/src/pages/Home.tsx
@@ -1,16 +1,37 @@
 import React from 'react';
 import { FaSearchLocation, FaLocationArrow } from 'react-icons/fa';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
 
 type Props = {};
 
 const Home = (props: Props) => {
+  const position = {lat:51.51669455487648, lng: -0.04810539546076163};
+
   return (
     <div className="home-container p-4 flex flex-col">
       <div className="searchBox relative border-2 rounded-full flex gap-1 items-center">
         <FaSearchLocation className=" absolute left-2 " />
-        <input type="text" name="location" id="location" placeholder='Search by Mosque or Postcode' className=' pl-8 pr-8 py-2 w-full rounded-full' />
-        <FaLocationArrow className=' absolute right-2' />
+        <input
+          type="text"
+          name="location"
+          id="location"
+          placeholder="Search by Mosque or Postcode"
+          className=" pl-8 pr-8 py-2 w-full rounded-full"
+        />
+        <FaLocationArrow className=" absolute right-2" />
       </div>
+      <MapContainer style={{width:"500px", height:"500px"}} center={position} zoom={13} scrollWheelZoom={false}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={position}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
     </div>
   );
 };
