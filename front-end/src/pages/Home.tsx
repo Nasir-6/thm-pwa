2c166bce3e081f493b7f360586c42135bd0e6f2d
Nasir <nasir-islam@hotmail.com>
2023-01-04 20:46:27 +0000

    bring back attribution but with flag removed

--- front-end/src/pages/Home.tsx
+++ front-end/src/pages/Home.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import { FaSearchLocation, FaLocationArrow } from 'react-icons/fa';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  AttributionControl,
+} from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 
 type Props = {};
 
 const Home = (props: Props) => {
-  const position = {lat:51.51669455487648, lng: -0.04810539546076163};
+  const position = { lat: 51.51669455487648, lng: -0.04810539546076163 };
 
   return (
     <div className="home-container p-4 flex flex-col">
@@ -21,16 +27,24 @@ const Home = (props: Props) => {
         />
         <FaLocationArrow className=" absolute right-2" />
       </div>
-      <MapContainer style={{width:"500px", height:"500px"}} center={position} zoom={13} scrollWheelZoom={false} attributionControl={false}>
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
+      <MapContainer
+        style={{ width: '500px', height: '500px' }}
+        center={position}
+        zoom={13}
+        scrollWheelZoom={false}
+        attributionControl={false}
+      >
         <Marker position={position}>
           <Popup>
-            Can insert some info about redcoat mosque here. Can insert pictures or text or even links here
+            Can insert some info about redcoat mosque here. Can insert pictures
+            or text or even links here
           </Popup>
         </Marker>
+        <TileLayer
+          attribution=' <a href="https://leafletjs.com/">Leaflet</a> | &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <AttributionControl position="bottomright" prefix={false} />
       </MapContainer>
     </div>
   );
