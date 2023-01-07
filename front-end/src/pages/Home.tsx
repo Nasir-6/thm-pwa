9daca29cbcdd663ef9e01d49324d44463803973e
Nasir <nasir-islam@hotmail.com>
2023-01-07 21:44:09 +0000

    create searchBar component

--- front-end/src/pages/Home.tsx
+++ front-end/src/pages/Home.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { FaSearchLocation, FaLocationArrow } from 'react-icons/fa';
 import Map from './map/Map';
+import SearchBar from './map/SearchBar';
 // import DataProcessor from '../data/DataProcessor';
 
 type Props = {};
@@ -9,17 +9,7 @@ const Home = (props: Props) => {
 
   return (
     <div className="home-container p-4 flex flex-col justify-center items-center">
-      <div className="searchBox relative border-2 rounded-full flex gap-1 items-center w-full max-w-xl mb-3">
-        <FaSearchLocation className=" absolute left-2 " />
-        <input
-          type="text"
-          name="location"
-          id="location"
-          placeholder="Search by Mosque or Postcode"
-          className=" pl-8 pr-8 py-2 w-full rounded-full"
-        />
-        <FaLocationArrow className=" absolute right-2" />
-      </div>
+      <SearchBar/>
       <Map/>
       {/* <DataProcessor/> */}
     </div>
