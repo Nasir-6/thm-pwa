48810e065ad39def27481aa524603b6ce883c40a
Nasir <nasir-islam@hotmail.com>
2023-01-07 21:42:07 +0000

    make Map component

--- front-end/src/pages/Home.tsx
+++ front-end/src/pages/Home.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import { FaSearchLocation, FaLocationArrow } from 'react-icons/fa';
-import { MapContainer, TileLayer, AttributionControl } from 'react-leaflet';
-import 'leaflet/dist/leaflet.css';
-import MosqueIcon from './map/MosqueIcon';
-import DataProcessor from '../data/DataProcessor';
+import Map from './map/Map';
+// import DataProcessor from '../data/DataProcessor';
 
 type Props = {};
 
 const Home = (props: Props) => {
-  const position = { lat: 51.51669455487648, lng: -0.04810539546076163 };
 
   return (
     <div className="home-container p-4 flex flex-col justify-center items-center">
@@ -23,21 +20,8 @@ const Home = (props: Props) => {
         />
         <FaLocationArrow className=" absolute right-2" />
       </div>
-      <MapContainer
-        style={{ width: '100%', height: '500px', zIndex: 0 }}
-        center={position}
-        zoom={13}
-        scrollWheelZoom={false}
-        attributionControl={false}
-      >
-        <TileLayer
-          attribution=' <a href="https://leafletjs.com/">Leaflet</a> | &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> | <a href="https://www.hotosm.org/" target="_blank">HOT</a> | <a href="https://openstreetmap.fr/" target="_blank">OpenStreetMap Fr</a>'
-          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
-        />
-        <MosqueIcon position={position} />
-        <AttributionControl position="bottomright" prefix={false} />
-      </MapContainer>
-      <DataProcessor/>
+      <Map/>
+      {/* <DataProcessor/> */}
     </div>
   );
 };
---
+++ front-end/src/pages/map/Map.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { MapContainer, TileLayer, AttributionControl } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import MosqueIcon from './MosqueIcon';
+
+const position = { lat: 51.51669455487648, lng: -0.04810539546076163 };
+
+type Props = {}
+
+const Map = (props: Props) => {
+  return (
+    <MapContainer
+        style={{ width: '100%', height: '500px', zIndex: 0 }}
+        center={position}
+        zoom={13}
+        scrollWheelZoom={false}
+        attributionControl={false}
+      >
+        <TileLayer
+          attribution=' <a href="https://leafletjs.com/">Leaflet</a> | &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> | <a href="https://www.hotosm.org/" target="_blank">HOT</a> | <a href="https://openstreetmap.fr/" target="_blank">OpenStreetMap Fr</a>'
+          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+        />
+        <MosqueIcon position={position} />
+        <AttributionControl position="bottomright" prefix={false} />
+      </MapContainer>
+  )
+}
+
+export default Map
