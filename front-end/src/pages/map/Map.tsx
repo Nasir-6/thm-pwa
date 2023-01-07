7a363ec6271f8811c54ca89004602bf4fe063c99
Nasir <nasir-islam@hotmail.com>
2023-01-07 21:48:30 +0000

    successfully import json data

--- front-end/src/pages/map/Map.tsx
+++ front-end/src/pages/map/Map.tsx
@@ -3,11 +3,13 @@ import { MapContainer, TileLayer, AttributionControl } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import MosqueIcon from './MosqueIcon';
 
+import mosqueData from '../../data/thm_mosques.json'
 const position = { lat: 51.51669455487648, lng: -0.04810539546076163 };
 
 type Props = {}
 
 const Map = (props: Props) => {
+    console.log('mosqueData', mosqueData)
   return (
     <MapContainer
         style={{ width: '100%', height: '500px', zIndex: 0 }}
