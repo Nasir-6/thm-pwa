fd023746bf1b9e973607d4dc90b4cc2cc13febfc
Nasir <nasir-islam@hotmail.com>
2023-01-08 14:16:51 +0000

    show all mosque icons in thm

--- front-end/src/pages/map/Map.tsx
+++ front-end/src/pages/map/Map.tsx
@@ -1,31 +1,41 @@
-import React from 'react'
+import React from 'react';
 import { MapContainer, TileLayer, AttributionControl } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import MosqueIcon from './MosqueIcon';
 
-import mosqueData from '../../data/thm_mosques.json'
+import mosqueData from '../../data/thm_mosques.json';
 const position = { lat: 51.51669455487648, lng: -0.04810539546076163 };
 
-type Props = {}
+type Props = {};
 
 const Map = (props: Props) => {
-    console.log('mosqueData', mosqueData)
+  console.log('mosqueData', mosqueData);
+  const createMosqueIcons = mosqueData.map((mosque) => {
+    console.log('mosque', mosque);
+    const mosquePos = {
+      lat: Number(mosque.Latitude),
+      lng: Number(mosque.Longitude),
+    };
+    return <MosqueIcon position={mosquePos} />;
+  });
+
   return (
     <MapContainer
-        style={{ width: '100%', height: '500px', zIndex: 0 }}
-        center={position}
-        zoom={13}
-        scrollWheelZoom={false}
-        attributionControl={false}
-      >
-        <TileLayer
-          attribution=' <a href="https://leafletjs.com/">Leaflet</a> | &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> | <a href="https://www.hotosm.org/" target="_blank">HOT</a> | <a href="https://openstreetmap.fr/" target="_blank">OpenStreetMap Fr</a>'
-          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
-        />
-        <MosqueIcon position={position} />
-        <AttributionControl position="bottomright" prefix={false} />
-      </MapContainer>
-  )
-}
+      style={{ width: '100%', height: '500px', zIndex: 0 }}
+      center={position}
+      zoom={13}
+      scrollWheelZoom={false}
+      attributionControl={false}
+    >
+      <TileLayer
+        attribution=' <a href="https://leafletjs.com/">Leaflet</a> | &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> | <a href="https://www.hotosm.org/" target="_blank">HOT</a> | <a href="https://openstreetmap.fr/" target="_blank">OpenStreetMap Fr</a>'
+        url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+      />
+      <MosqueIcon position={position} />
+      {createMosqueIcons}
+      <AttributionControl position="bottomright" prefix={false} />
+    </MapContainer>
+  );
+};
 
-export default Map
+export default Map;
