ef6e42824bf4987d098dd210828bb911da2354f4
Nasir <nasir-islam@hotmail.com>
2023-01-13 20:54:49 +0000

    extract csvfilename as a variable and add todos

--- time_converter_scripts/CSVTimesConverter.py
+++ time_converter_scripts/CSVTimesConverter.py
@@ -3,8 +3,6 @@ import os
 from timeConverters import fajr_convert, zuhr_convert, asr_convert, maghrib_convert, isha_convert, period_to_colon
 
 
-
-# newpath = r'C:\Program Files\arbitrary'
 cd = os.getcwd()
 newFolderPath = cd + "/mosques-new"
 if not os.path.exists(newFolderPath):
@@ -12,17 +10,23 @@ if not os.path.exists(newFolderPath):
 
 
 # Read in one csv file and make a new copy of it and adjust the values by writing to a new csv file
-with open("./Apr-2022.csv", 'r') as file:
+csvFileName = "Apr-2022.csv"
+
+# TODO: Extract this part into a function which takes in csvFileName
+# TODO: learn to loop through mosque folder and the individual mosque folders
+# TODO: create a copy of just the folders
+# TODO: Adjust the extracted function to place the new copy in the copied mosque-new folders
+with open("./" + csvFileName, 'r') as file:
   csvreader = csv.DictReader(file)
 
-  with open("./mosques-new/Apr-2022-New.csv", 'w') as newFile:
+  with open(newFolderPath + "/" + csvFileName, 'w') as newFile:
     fieldNames = csvreader.fieldnames
     csvWriter = csv.DictWriter(newFile, fieldnames=fieldNames)
     csvWriter.writeheader()
     for row in csvreader:
       newRow = {}
       for name in fieldNames:
-        if(name.upper() == "DAY" or name.upper() == "DATE"):
+        if name.upper() == "DAY" or name.upper() == "DATE":
           newRow[name] = row[name]
           # continue to prevent issue with period_to_colon function
           continue
@@ -31,7 +35,7 @@ with open("./Apr-2022.csv", 'r') as file:
         if name.upper() == "FAJR":
           newRow[name] = fajr_convert(row[name])
 
-        elif name.upper() == "ZOHAR": # spelt zohar in csvs!
+        elif name.upper() == "ZOHAR":   # Spelt zohar in csvs!
           newRow[name] = zuhr_convert(row[name])
 
         elif name.upper() == "ASR":
@@ -40,7 +44,7 @@ with open("./Apr-2022.csv", 'r') as file:
         elif name.upper() == "MAGHRIB":
           newRow[name] = maghrib_convert(row[name])
 
-        elif name.upper() == "ESHA": #Spelt esha in csvs
+        elif name.upper() == "ESHA":    # Spelt esha in csvs
           newRow[name] = isha_convert(row[name])
 
         else:
