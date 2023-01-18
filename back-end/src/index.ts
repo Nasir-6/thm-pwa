16ed1a895fe5350abd77962c393e87bd3e5646dc
Nasir <nasir-islam@hotmail.com>
2023-01-18 16:18:16 +0000

    install pg and configure and run test with pokemon db

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -1,12 +1,22 @@
 /* eslint-disable @typescript-eslint/no-misused-promises */
 import express from "express";
 import cors from "cors";
+import pool from "./db";
 
 const app = express();
 app.use(cors());
 
 app.get("/", async (req, res) => {
 	res.json({ test: "success" });
+});
+
+app.get("/pg", async (req, res) => {
+	try {
+		const testList = await pool.query("SELECT * FROM pokedex;");
+		res.json(testList.rows);
+	} catch (err) {
+		console.log("err", err);
+	}
 });
 
 const PORT = process.env.PORT || 8080;
