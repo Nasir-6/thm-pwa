cafc85766c6d637353a5c8b4d4847f01924dc914
Nasir <nasir-islam@hotmail.com>
2023-01-23 22:26:42 +0000

    add timetable path

--- back-end/src/routes/mosques.ts
+++ back-end/src/routes/mosques.ts
@@ -6,4 +6,8 @@ router.get("/", (req, res) => {
 	res.send("MOSQUES SUCCESSFUL");
 });
 
+router.get("/timetable", (req, res) => {
+	res.send("MOSQUES Timetable SUCCESSFUL");
+});
+
 export default router;
