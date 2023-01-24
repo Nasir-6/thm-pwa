2e7be158f867a6750624fc13a2caa79904ca2d68
Nasir <nasir-islam@hotmail.com>
2023-01-24 19:10:28 +0000

    increase prettier printwidth to 145

--- back-end/src/db/mosqueDb.ts
+++ back-end/src/db/mosqueDb.ts
@@ -5,8 +5,5 @@ export interface MosqueDb {
 	getAllMosques(): Promise<MosqueDto[]>;
 	getMosqueByUid(uid: string): Promise<MosqueDto>;
 	getTimesForAGivenDateForAllMosques(date: Date): Promise<DailyTimesDto[]>;
-	getTimesForAGivenDateForAMosque(
-		mosqueUid: string,
-		date: Date
-	): Promise<DailyTimesDto>;
+	getTimesForAGivenDateForAMosque(mosqueUid: string, date: Date): Promise<DailyTimesDto>;
 }
