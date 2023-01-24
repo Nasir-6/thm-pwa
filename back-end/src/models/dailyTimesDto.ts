3aa4924b2714181c3f07628bb8cbfb8b2b2a5872
Nasir <nasir-islam@hotmail.com>
2023-01-24 18:53:35 +0000

    add mosque and dailytime dtos/models

---
+++ back-end/src/models/dailyTimesDto.ts
@@ -0,0 +1,11 @@
+export type DailyTimesDto = {
+	uid: string;
+	mosqueUid: string;
+	mosqueName: string;
+	date: Date;
+	fajr: Date;
+	zuhr: Date;
+	asr: Date;
+	maghrib: Date;
+	isha: Date;
+};
---
+++ back-end/src/models/mosqueDto.ts
@@ -0,0 +1,8 @@
+export type MosqueDto = {
+	uid: string;
+	name: string;
+	address: string;
+	latitude: number;
+	longitude: number;
+	googleUrl: string;
+};
