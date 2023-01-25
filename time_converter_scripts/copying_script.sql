acecea198bbc3d5a2188ca0725a37ab555168291
Nasir <nasir-islam@hotmail.com>
2023-01-25 22:18:07 +0000

    change name to mosque_name

--- time_converter_scripts/copying_script.sql
+++ time_converter_scripts/copying_script.sql
@@ -18,7 +18,7 @@ CSV HEADER;
 CREATE TABLE mosque_times (
 	id SERIAL UNIQUE PRIMARY KEY,
 	mosque_id INTEGER REFERENCES mosques(id),
-	name VARCHAR NOT NULL,
+	mosque_name VARCHAR NOT NULL,
 	date DATE NOT NULL,
 	fajr TIME NOT NULL,
 	zuhr TIME NOT NULL,
@@ -27,21 +27,40 @@ CREATE TABLE mosque_times (
 	isha TIME NOT NULL
 );
 
-
-COPY mosque_times(mosque_id, name, date, fajr, zuhr, asr, maghrib, isha)
+COPY mosque_times(mosque_id, mosque_name, date, fajr, zuhr, asr, maghrib, isha)
 FROM '/Users/nasir/thm-pwa/time_converter_scripts/chatGPT-mosques-new-DB-cleaned/all_mosque_combined.csv'
 DELIMITER ','
 CSV HEADER;
 
 
+DROP TABLE salah_beginning_times;
+
+CREATE TABLE salah_beginning_times (
+	id SERIAL UNIQUE PRIMARY KEY,
+	date DATE UNIQUE NOT NULL,
+	fajr TIME NOT NULL,
+	zuhr TIME NOT NULL,
+	asr_1st_mithl TIME NOT NULL,
+	asr_2nd_mithl TIME NOT NULL,
+	maghrib TIME NOT NULL,
+	isha TIME NOT NULL,
+	sunrise TIME NOT NULL
+);
+
+COPY salah_beginning_times(date, fajr, zuhr, asr_1st_mithl, maghrib, isha, sunrise, asr_2nd_mithl)
+FROM '/Users/nasir/thm-pwa/time_converter_scripts/salah_beginning_combined.csv'
+DELIMITER ','
+CSV HEADER;
+
+
 DROP TABLE postcodes;
+
 CREATE TABLE postcodes (
     id SERIAL UNIQUE PRIMARY KEY,
 	postcode VARCHAR UNIQUE,
     latitude NUMERIC NOT NULL,
 	longitude NUMERIC NOT NULL
 );
-
 
 COPY postcodes(postcode, latitude, longitude)
 FROM '/Users/nasir/thm-pwa/time_converter_scripts/th_postcodes.csv'
