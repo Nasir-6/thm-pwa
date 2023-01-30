2656e834aee8fcd42a53c7160deda4b0f9074507
Nasir <nasir-islam@hotmail.com>
2023-01-30 01:06:48 +0000

    minor eslintCOnfig

--- back-end/src/middlewares/ErrorHandler.ts
+++ back-end/src/middlewares/ErrorHandler.ts
@@ -1,9 +1,12 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { NextFunction, Request, Response } from "express";
 import HttpException from "../exceptions/httpExceptions";
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 const ErrorHandler = (err: HttpException | any, req: Request, res: Response, next: NextFunction) => {
+	// eslint-disable-next-line no-console
 	console.log("Handling Error via MiddleWare...");
 	// console.log("err.stack", err.stack);
 	const errStatus: number = err.status || err.statusCode || 500;
