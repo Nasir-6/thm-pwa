9a7927b011391772cb1ab6b38262ebdd0553c59d
Nasir <nasir-islam@hotmail.com>
2023-01-31 22:01:59 +0000

    get closest salah time

--- front-end/src/pages/map/DateTimeTest.tsx
+++ front-end/src/pages/map/DateTimeTest.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { useQuery } from 'react-query';
+import { closestIndexTo } from 'date-fns';
 import { getTimesForAMosqueOnAGivenDate } from '../../api/mosques';
+
+const salahArr = ['Fajr', 'Zuhr', 'Asr', 'Maghrib', 'Isha'];
 
 const DateTimeTest = () => {
   const { data: mosqueDailyTimes, isSuccess } = useQuery({
@@ -8,6 +11,17 @@ const DateTimeTest = () => {
     queryFn: () => getTimesForAMosqueOnAGivenDate(39, new Date()),
     // staleTime: 1000 * 60 * 10, // TODO: Change this to ms until midnight! - setup a Util function
   });
+
+  let closestSalah = 'None';
+  let closestTime = new Date();
+  if (isSuccess) {
+    const { fajr, zuhr, asr, maghrib, isha } = mosqueDailyTimes;
+    const timeArr = [fajr, zuhr, asr, maghrib, isha];
+    const closestIndex = closestIndexTo(new Date(), timeArr);
+    closestSalah = closestIndex ? salahArr[closestIndex] : 'No Closest Salah found!';
+    closestTime = closestIndex ? timeArr[closestIndex] : closestTime;
+  }
+
   return (
     <div>
       <h1>DateTimeTest</h1>
@@ -21,6 +35,8 @@ const DateTimeTest = () => {
           <p>{`Maghrib: ${mosqueDailyTimes.maghrib.toLocaleTimeString()}`}</p>
           <p>{`Isha: ${mosqueDailyTimes.isha.toLocaleTimeString()}`}</p>
           <p>{new Date().toLocaleString()}</p>
+          <p>{`Closest Salah to current time is: ${closestSalah} `}</p>
+          <p>{`Closest Time to current time is: ${closestTime.toLocaleTimeString()} `}</p>
         </div>
       ) : (
         <p>Failed to fetch</p>
