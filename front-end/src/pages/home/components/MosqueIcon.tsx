97fa8ee68617a7ab88b41945638e041c0741531e
Nasir <nasir-islam@hotmail.com>
2023-02-06 20:47:52 +0000

    make mapIcon into svg and also use theme colors

--- front-end/src/pages/home/components/MosqueIcon.tsx
+++ front-end/src/pages/home/components/MosqueIcon.tsx
@@ -23,12 +23,12 @@ const MosqueIcon: React.FC<Props> = ({ mosque }) => {
       xmlns="http://www.w3.org/2000/svg"
       d="M14,0 C21.732,0 28,5.641 28,12.6 C28,23.963 14,36 14,36 C14,36 0,24.064 0,12.6 C0,5.641 6.268,0 14,0 Z"
       id="Shape"
-      fill="#FF6E6E"
+      fill="#ea7200"
     ></path>
     <circle
       xmlns="http://www.w3.org/2000/svg"
       id="Oval"
-      fill="#0C0058"
+      fill="#163037"
       fill-rule="nonzero"
       cx="14"
       cy="14"
