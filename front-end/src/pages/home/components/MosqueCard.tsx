490a26cf0bd454505868dc06521f27df455f262c
Nasir <nasir-islam@hotmail.com>
2023-02-06 23:22:31 +0000

    Minor style edits

--- front-end/src/pages/home/components/MosqueCard.tsx
+++ front-end/src/pages/home/components/MosqueCard.tsx
@@ -5,22 +5,22 @@ import { ReactComponent as MapIcon } from '../../../assets/mapIcon.svg';
 const MosqueCard = () => {
   console.log('Hello');
   return (
-    <div className="mosque-card flex gap-2 justify-center px-1 py-3 border-t w-fit self-center">
-      <div className="map-details flex flex-col items-center justify-center px-1 py-2">
+    <div className="mosque-card flex gap-3 justify-center px-2 py-3 border-t w-fit self-center">
+      <div className="map-details flex flex-col items-center justify-center py-2 text-xs text-slate-400">
         <MapIcon width="25px" height="30px" />
-        <p className="distance text-xs text-slate-500 ">0.79</p>
-        <p className="distance text-xs text-slate-500">Miles</p>
+        <p className="distance">0.79</p>
+        <p className="distance">Miles</p>
       </div>
 
-      <div className="mosque-details flex flex-col text-left p-1 gap-1 items-start">
-        <h3 className="mosque-name font-semibold text-ellipsis">Redcoat Community Center & Mosque</h3>
-        <p className="address text-sm text-slate-500">256 Stepney Way, London, E1 3DW</p>
+      <div className="mosque-details flex flex-col text-left py-1 gap-0.5 items-start">
+        <h3 className="mosque-name font-semibold">Redcoat Community Center & Mosque</h3>
+        <p className="address text-sm text-slate-400">256 Stepney Way, London, E1 3DW</p>
         <button type="button" className="view-details-btn font-semibold text-accent-700 hover:text-accent-800 text-sm">
           View Details
         </button>
       </div>
 
-      <div className="next-salah p-1 flex flex-col items-center justify-center min-w-fit">
+      <div className="next-salah py-1 flex flex-col items-center justify-center min-w-fit">
         <p className="font-bold text-xs text-primary-700">Isha</p>
         <p className="font-bold text-xs text-primary-700">07:30 PM</p>
       </div>
