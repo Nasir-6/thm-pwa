a8da7292b5392b8a5a334d3270f2d0de463e22ad
Nasir <nasir-islam@hotmail.com>
2023-02-07 14:33:23 +0000

    standardise mosqueCard width

--- front-end/src/pages/home/components/MosqueCard.tsx
+++ front-end/src/pages/home/components/MosqueCard.tsx
@@ -57,14 +57,14 @@ const MosqueCard: React.FC<Props> = ({ mosque }) => {
   }, [mosqueTimesToday, mosqueTimesTomorrow]);
 
   return (
-    <div className="mosque-card flex gap-3 justify-center px-2 py-3 border-t w-fit self-center">
+    <div className="mosque-card flex gap-3 justify-center px-2 py-3 border-t w-full max-w-xl self-center">
       <div className="map-details flex flex-col items-center justify-center py-2 text-xs text-slate-400">
         <MapIcon width="25px" height="30px" />
         <p className="distance">{mosque.distanceToLocationInMiles}</p>
         <p className="distance">Miles</p>
       </div>
 
-      <div className="mosque-details flex flex-col text-left py-1 gap-0.5 items-start">
+      <div className="mosque-details flex flex-col text-left py-1 gap-0.5 items-start w-full">
         <h3 className="mosque-name font-semibold">{mosque.name}</h3>
         <p className="address text-sm text-slate-400">{mosque.address}</p>
         <button type="button" className="view-details-btn font-semibold text-accent-700 hover:text-accent-800 text-sm">
