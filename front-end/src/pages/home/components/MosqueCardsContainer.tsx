2031049ed22b5ddfea015d3b86b1281a4cd9e323
Nasir <nasir-islam@hotmail.com>
2023-02-08 20:20:29 +0000

    setup dropdown for pagination

--- front-end/src/pages/home/components/MosqueCardsContainer.tsx
+++ front-end/src/pages/home/components/MosqueCardsContainer.tsx
@@ -11,33 +11,51 @@ interface MosqueCardsContainerProps {
 
 const MosqueCardsContainer: React.FC<MosqueCardsContainerProps> = ({ mosques }) => {
   const userLocation = useStore((state) => state.userLocation);
-  // Code for locations - but haven't calculated distances
-  const [location, setLocation] = useState<Position>({
+  const defaultLocation: Position = {
     name: 'East London Mosque',
     latitude: 51.51757166,
     longitude: -0.06548708235,
-  });
+  };
+  const [chosenLocation, setChosenLocation] = useState<Position>(defaultLocation);
+
   useEffect(() => {
     if (userLocation === undefined) return;
-    setLocation(userLocation);
-  }, [userLocation]); // Change this to the useLocation button
+    setChosenLocation(userLocation);
+  }, [userLocation]);
 
   const [sortedMosquesArr, setSortedMosquesArr] = useState<MosqueDTO[] | undefined>();
   useEffect(() => {
     if (mosques === undefined) return;
     const updatedLocationMosques = mosques.map((mosque) => ({
       ...mosque,
-      distanceToLocationInMiles: getDistanceToMosqueFromUserLocation(location, mosque.latitude, mosque.longitude),
+      distanceToLocationInMiles: getDistanceToMosqueFromUserLocation(chosenLocation, mosque.latitude, mosque.longitude),
     }));
     const sortedMosques = updatedLocationMosques.sort((a, b) => (a.distanceToLocationInMiles > b.distanceToLocationInMiles ? 1 : -1));
     setSortedMosquesArr(sortedMosques);
-  }, [location, mosques]);
+  }, [chosenLocation, mosques]);
 
   const createMosqueCards = sortedMosquesArr?.map((mosque) => <MosqueCard mosque={mosque} />);
 
   return (
     <>
-      <p>Mosque Card Container</p>
+      <div className="list-info w-full max-w-xl flex justify-between p-3">
+        {/* Need to sort out loading and empty states!! */}
+        {mosques === undefined ? (
+          <p>No mosques</p>
+        ) : (
+          <p className="location-identifier text-slate-500">{`${mosques?.length} Mosques Near "${chosenLocation.name}"`}</p>
+        )}
+        <form>
+          <label htmlFor="numOfMosques">
+            Showing
+            <select id="numOfMosques">
+              <option value="5">5</option>
+              <option value="10">10</option>
+              <option value="15">15</option>
+            </select>
+          </label>
+        </form>
+      </div>
       {createMosqueCards}
     </>
   );
