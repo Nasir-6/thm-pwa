1960d0157f25edc6e403d8359d039b83ae4d91f9
Nasir <nasir-islam@hotmail.com>
2023-02-08 23:53:36 +0000

    add left and right buttons

--- front-end/src/pages/home/components/Pagination.tsx
+++ front-end/src/pages/home/components/Pagination.tsx
@@ -11,20 +11,39 @@ const Pagination: React.FC<PaginationProps> = ({ mosquesPerPage, totalMosques, c
   // TODO: Deal with edge case when say go to last page (when mosquesPerPage is 5 but then change to 10 - reducing pages to half - but now current page doesn't exist)
   const pageNumbers = [];
 
-  for (let i = 1; i <= Math.ceil(totalMosques / mosquesPerPage); i++) {
+  const maxPageNum = Math.ceil(totalMosques / mosquesPerPage);
+  for (let i = 1; i <= maxPageNum; i++) {
     pageNumbers.push(i);
   }
+
+  //   TODO: Add left and right buttons
   return (
-    <div className="pagination flex">
+    <div className="pagination flex font-semibold">
+      <button
+        type="button"
+        onClick={() => {
+          if (currentPage !== 1) setCurrentPage(currentPage - 1);
+        }}
+        className="pagination-btn py-1 px-2 rounded">
+        {'<'}
+      </button>
       {pageNumbers.map((num) => (
         <button
           key={num}
           type="button"
           onClick={() => setCurrentPage(num)}
-          className={`pagination-btn font-semibold py-1 px-2 rounded ${num === currentPage ? 'bg-primary-600 text-white' : ''}`}>
+          className={`pagination-btn py-1 px-2 rounded ${num === currentPage ? 'bg-primary-600 text-white' : ''}`}>
           {num}
         </button>
       ))}
+      <button
+        type="button"
+        onClick={() => {
+          if (currentPage !== maxPageNum) setCurrentPage(currentPage + 1);
+        }}
+        className="pagination-btn py-1 px-2 rounded">
+        {'>'}
+      </button>
     </div>
   );
 };
