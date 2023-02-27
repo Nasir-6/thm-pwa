35da18b2554b144100fb12e8b54e530c1f8de40e
Nasir <nasir-islam@hotmail.com>
2023-02-27 23:50:41 +0000

    fix issue with rotated img and also add user input and print statment to communicate with user

--- timetable_scanner/main.py
+++ timetable_scanner/main.py
@@ -1,13 +1,21 @@
 import cv2
-from PIL import Image
+from PIL import Image, ImageOps
 import pytesseract
 
 img_path = "Feb-2023.JPG"
 
-img = Image.open(img_path)
+im = Image.open(img_path)
+# To prevent img from being auto rotated due to exif value
+# https://github.com/python-pillow/Pillow/issues/4703
+im = ImageOps.exif_transpose(im)
+im.show("Feb-2023")
+usr_res = input("Does the image look fine (y/n)?")
+if(usr_res == "n"):
+    exit("NOPE IMAGE IS NOT GOOD")
+
 
 print("Converting image to string....")
-ocr_result = pytesseract.image_to_string(img)
+ocr_result = pytesseract.image_to_string(im)
 
 print("Printing Results")
 print(ocr_result)
