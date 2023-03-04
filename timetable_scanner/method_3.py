d1bdb717217d36704dac50567720ec714be3c742
Nasir <nasir-islam@hotmail.com>
2023-03-04 23:59:30 +0000

    extract horizontal and vertical lines

---
+++ timetable_scanner/method_3.py
@@ -0,0 +1,53 @@
+# This one will try to draw the horizontal and vertical table lines by:
+# By Diluting the times and making each row/column one blob to be able to draw the row/column lines
+# Then use those to identify blocks!
+
+import cv2
+from PIL import Image, ImageOps
+import pytesseract
+import numpy as np
+from util import showImgInWindow, returnGrayscaleImg, showImgAndReturnIfMeetsCriteria, getThresholdFromUserInput
+
+# Step 1 : First read in file and make grayscale, also grab img shape, copy and store a base_img for later, show img
+# img_path = "IMG_4350.JPG"
+img_path = "Redcoat-Mar-23.jpg"
+im = Image.open(img_path)
+im = ImageOps.exif_transpose(im)
+
+# SO can pass display image
+im = np.asarray(im)
+base_image = im.copy()
+im_h, im_w, im_d = im.shape
+
+# showImgInWindow(im, "pre-processed PIL img")
+gray_img = returnGrayscaleImg(im)
+showImgInWindow(gray_img, "greyscaled Image")
+
+# Step 2: Binarise img - pick a threshold such that table lines can be seen!
+# Use threshold of ~ 50 so faint table lines don't show - want only text and smudge text!
+imgMeetsCriteria = False
+while not imgMeetsCriteria:
+    usrThreshold = getThresholdFromUserInput()
+    ret, thresh_value = cv2.threshold(gray_img, usrThreshold, 255, cv2.THRESH_BINARY_INV)
+    print("Does current threshold of '" + str(usrThreshold) + "' meet the criteria?")
+    imgMeetsCriteria = showImgAndReturnIfMeetsCriteria(thresh_value, "Binary Img")
+
+
+# Step 3: Dilate all the text horizontally so as to produce singular blob allowing for row identification
+kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (50, 5))     # Use 50 to spread in x and join times, but 5 in y so as to add padding in y to box
+dilated_value = cv2.dilate(thresh_value,kernel,iterations = 5)
+showImgInWindow(dilated_value, "Dilated img")
+
+# Draw contours
+contours, hierarchy = cv2.findContours(dilated_value, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+cordinates = []
+for cnt in contours:
+    x, y, w, h = cv2.boundingRect(cnt)
+    # bounding the images
+    if w > im_w * 0.95:
+        cv2.rectangle(im, (x, y), (x + w, y + h), (0, 0, 255), 3)
+        # Only append the ones we want/drew boxes around!!!
+        cordinates.append((x, y, w, h))
+        # cordinates.insert(0, (x, y, w, h))
+
+showImgInWindow(im, "Final image with contour")
--- timetable_scanner/new_method.py
+++ timetable_scanner/new_method.py
@@ -1,5 +1,7 @@
 # This one will try to draw the horizontal and vertical table lines
 # Then use those to identify blocks!
+
+# ISSUE WIth this is if the border lines are too thin and can't be picked up on!!!
 import cv2
 from PIL import Image, ImageOps
 import pytesseract
@@ -7,7 +9,8 @@ import numpy as np
 from util import showImgInWindow, returnGrayscaleImg, showImgAndReturnIfMeetsCriteria, getThresholdFromUserInput
 
 # Step 1 : First read in file and make grayscale, also grab img shape, copy and store a base_img for later, show img
-img_path = "IMG_4350.JPG"
+# img_path = "IMG_4350.JPG"
+img_path = "Redcoat-Mar-23.jpg"
 
 im = Image.open(img_path)
 im = ImageOps.exif_transpose(im)
@@ -28,3 +31,47 @@ while not imgMeetsCriteria:
     ret, thresh_value = cv2.threshold(gray_img, usrThreshold, 255, cv2.THRESH_BINARY_INV)
     print("Does current threshold of '" + str(usrThreshold) + "' meet the criteria?")
     imgMeetsCriteria = showImgAndReturnIfMeetsCriteria(thresh_value, "Binary Img")
+
+
+# Step 3: Extract all vertical lines by eroding anything apart from vertical lines
+
+
+horizontal_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (np.array(im).shape[1] // 150, 1))
+eroded_image = cv2.erode(thresh_value, horizontal_kernel, iterations=5)
+showImgAndReturnIfMeetsCriteria(eroded_image, "eroded img")
+
+horizontal_lines = cv2.dilate(eroded_image, horizontal_kernel, iterations=5)
+showImgInWindow(horizontal_lines, "Vertical lines after dilation")
+
+vertical_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (1, im_w//150))
+eroded_image = cv2.erode(thresh_value, vertical_kernel, iterations=5)
+showImgAndReturnIfMeetsCriteria(eroded_image, "eroded img")
+
+vertical_lines = cv2.dilate(eroded_image, vertical_kernel, iterations=5)
+showImgInWindow(vertical_lines, "Vertical lines after dilation")
+
+
+# Combining
+kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (2, 2))
+vertical_horizontal_lines = cv2.addWeighted(vertical_lines, 0.5, horizontal_lines, 0.5, 0.0)
+vertical_horizontal_lines = cv2.erode(~vertical_horizontal_lines, kernel, iterations=3)
+
+
+thresh, vertical_horizontal_lines = cv2.threshold(vertical_horizontal_lines,128,255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+b_image = cv2.bitwise_not(cv2.bitwise_xor(im,vertical_horizontal_lines))
+
+showImgInWindow(vertical_horizontal_lines, "combo")
+
+# Draw contours
+contours, hierarchy = cv2.findContours(vertical_horizontal_lines, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+cordinates = []
+for cnt in contours:
+    x, y, w, h = cv2.boundingRect(cnt)
+    # bounding the images
+    if True or w > im_w * 0.7:
+        cv2.rectangle(im, (x, y), (x + w, y + h), (0, 0, 255), 3)
+        # Only append the ones we want/drew boxes around!!!
+        cordinates.append((x, y, w, h))
+        # cordinates.insert(0, (x, y, w, h))
+
+showImgInWindow(im, "Final image with contour")
