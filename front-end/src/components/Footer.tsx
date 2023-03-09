f8d0e210d8f992fd7ca90995a7366ed5e90a0b88
Nasir <nasir-islam@hotmail.com>
2023-03-09 23:30:06 +0000

    change copyright from 2023 to 2016

--- front-end/src/components/Footer.tsx
+++ front-end/src/components/Footer.tsx
@@ -3,7 +3,7 @@ import { FaFacebook, FaTwitter, FaInstagram } from 'react-icons/fa';
 
 const Footer = () => (
   <footer className="footer bg-gradient-to-r from-primary-600 to-primary-800 text-white p-4 flex flex-col sm:flex-row gap-2 justify-center items-center">
-    <p className="copyright-text text-xs font-bold">Copyright © 2023 Tower Hamlets Mosques, All rights reserved.</p>
+    <p className="copyright-text text-xs font-bold">Copyright © 2016 Tower Hamlets Mosques, All rights reserved.</p>
     <div className="social-icons flex">
       <a href="https://www.facebook.com/thmuslims">
         <FaFacebook className="w-8 hover:text-accent-600" />
