c3e29128e987cc0b1de398b5c03ebdc9969849b8
Nasir <nasir-islam@hotmail.com>
2023-03-11 19:46:33 +0000

    setup new mosqueCard format

--- front-end/src/pages/home/components/MosqueCard.tsx
+++ front-end/src/pages/home/components/MosqueCard.tsx
@@ -11,8 +11,8 @@ const MosqueCard: React.FC<Props> = ({ mosque }) => (
   <div className="mosque-card flex gap-3 justify-center px-2 py-3 border-t w-full max-w-xl self-center">
     <div className="map-details flex flex-col items-center justify-center text-xs text-slate-400">
       <MapIcon width="25px" height="30px" />
-      <p className="distance">{mosque.distanceToLocationInMiles}</p>
-      <p className="distance">Miles</p>
+      {/* <p className="distance">{mosque.distanceToLocationInMiles}</p>
+      <p className="distance">Miles</p> */}
     </div>
 
     <div className="mosque-details flex flex-col text-left py-1 gap-0.5 items-start w-full">
@@ -21,6 +21,10 @@ const MosqueCard: React.FC<Props> = ({ mosque }) => (
       <button type="button" className="view-details-btn font-semibold text-accent-700 hover:text-accent-800 text-sm">
         View Details
       </button>
+    </div>
+
+    <div className="map-details flex items-start text-xs text-slate-400 min-w-fit py-1">
+      <p className="distance">{mosque.distanceToLocationInMiles} Miles</p>
     </div>
   </div>
 );
