32d59fda2ed9921ee4f306fdb9e24c3e14a2a564
Nasir <nasir-islam@hotmail.com>
2023-03-14 23:37:30 +0000

    get basic skeleton in

--- front-end/src/pages/home/components/SalahBeginningModal.tsx
+++ front-end/src/pages/home/components/SalahBeginningModal.tsx
@@ -14,8 +14,55 @@ const SalahBeginningModal: React.FC<SalahBeginningModalProps> = ({ setIsModalSho
       aria-hidden="true"
       role="button"
       className="dark-overlay bg-slate-900 bg-opacity-25 fixed w-full h-full top-0 z-20 flex justify-center items-center overflow-hidden">
-      <div className="salah-beginning-modal absolute bg-white">
-        <h2>Salah Beginning Times</h2>
+      <div className="salah-beginning-modal absolute bg-white max-w-lg w-full">
+        <div className="header flex">
+          <h2 className="">Salah Beginning Times</h2>
+          <p>X</p>
+        </div>
+        <div className="current-info">
+          <div className="next-salah">
+            <p>Icon</p>
+            <p>Time Now</p>
+            <p>Time to Next Salaah</p>
+          </div>
+          <div className="date-picker">Date Picker Here</div>
+        </div>
+
+        <div className="fajr">
+          <p>Icon</p>
+          <p>Fajr</p>
+          <p>Time</p>
+        </div>
+        <div className="fajr">
+          <p>Icon</p>
+          <p>Fajr</p>
+          <p>Time</p>
+        </div>
+        <div className="fajr">
+          <p>Icon</p>
+          <p>Fajr</p>
+          <p>Time</p>
+        </div>
+        <div className="fajr">
+          <p>Icon</p>
+          <p>Fajr</p>
+          <p>Time</p>
+        </div>
+        <div className="fajr">
+          <p>Icon</p>
+          <p>Fajr</p>
+          <p>Time</p>
+        </div>
+        <div className="fajr">
+          <p>Icon</p>
+          <p>Fajr</p>
+          <p>Time</p>
+        </div>
+        <div className="fajr">
+          <p>Icon</p>
+          <p>Fajr</p>
+          <p>Time</p>
+        </div>
       </div>
     </div>,
     document.body
