80f0e100d2368a0e77a7fdd4d13dedb82ea1dd09
Nasir <nasir-islam@hotmail.com>
2023-06-11 23:57:48 +0100

    fix issue with window not being defined due to not running in client side code only - so move to useEffect

--- next-front-end/src/app/page.tsx
+++ next-front-end/src/app/page.tsx
@@ -7,9 +7,11 @@ import React, { Suspense, useEffect, useState } from 'react';
 export default function Home() {
   const [isMapVisible, setIsMapVisible] = useState(false);
   const screenBreakPoint = 1024;
-  const [isDesktopView, setIsDesktopView] = useState(window.innerWidth > screenBreakPoint);
+  const [isDesktopView, setIsDesktopView] = useState(false);
 
   useEffect(() => {
+    // Can only access window here in useEffect as this is run on client side on mount
+    setIsDesktopView(window.innerWidth > screenBreakPoint);
     const mediaQuery = window.matchMedia('(min-width: 1024px)');
     mediaQuery.addEventListener('change', () => setIsDesktopView(mediaQuery.matches));
     // this is the cleanup function to remove the listener
@@ -38,7 +40,12 @@ export default function Home() {
         <>
           <ControlPanel isMapVisible={isMapVisible} setIsMapVisible={setIsMapVisible} />
           <div className={`map-div ${isMapVisible ? '' : 'map-div-hide'}`}>
-            <Suspense fallback={<Skeleton type="map" />}>{isMapVisible && <Map isMapVisible={isMapVisible} />}</Suspense>
+            <Suspense fallback={<Skeleton type="map" />}>
+              {
+                isMapVisible && <h1>Map would go here</h1>
+                // <Map isMapVisible={isMapVisible} />
+              }
+            </Suspense>
           </div>
           <h1>MosqueResultsContainer would go here</h1>
           {/* <MosqueResultsContainer /> */}
