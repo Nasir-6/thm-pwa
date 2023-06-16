2f9b4d40ba87efe78f9a6ab6259c71e88027a5d8
Nasir <nasir-islam@hotmail.com>
2023-06-16 23:46:04 +0100

    add dropdown base functionality

--- next-front-end/src/components/MosquesByArea.tsx
+++ next-front-end/src/components/MosquesByArea.tsx
@@ -1,9 +1,11 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
 // This is a serverComponent - as mosques should have been fetched on server
 // Will use only CSS to make dropdown (eliminate need to make this a client component due to JS)
 /* eslint-disable react/no-unescaped-entities */
 import Link from 'next/link';
 // eslint-disable-next-line import/no-relative-packages
 import { MosqueDTO } from '../../../back-end/src/db/models/mosques';
+import './MosquesByArea.css';
 
 interface Props {
   mosques: MosqueDTO[];
@@ -17,8 +19,23 @@ const MosquesByArea = ({ mosques }: Props) => {
     </div>
   ));
 
+  const inputId = 'area-toggle';
+
   // eslint-disable-next-line react/jsx-no-useless-fragment
-  return <>{mosquesList}</>;
+  return (
+    <>
+      {mosquesList}
+      <label htmlFor={inputId} className="area-toggle-label">
+        Stepney
+      </label>
+      <input type="checkbox" id={inputId} className="area-toggle" />
+      <ul className="flex flex-col">
+        <Link href="https://www.towerhamletsmosques.co.uk/redcoatmasjid">Redcoat</Link>
+        <Link href="https://www.towerhamletsmosques.co.uk/redcoatmasjid">Redcoat</Link>
+        <Link href="https://www.towerhamletsmosques.co.uk/redcoatmasjid">Redcoat</Link>
+      </ul>
+    </>
+  );
 };
 
 export default MosquesByArea;
