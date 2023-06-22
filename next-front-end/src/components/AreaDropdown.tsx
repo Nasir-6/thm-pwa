824d410057095095a04b5d09417beb72b29c90ef
Nasir <nasir-islam@hotmail.com>
2023-06-22 23:10:43 +0100

    add cross/plus svg icon

--- next-front-end/src/components/AreaDropdown.tsx
+++ next-front-end/src/components/AreaDropdown.tsx
@@ -4,6 +4,7 @@ import Link from 'next/link';
 // eslint-disable-next-line import/no-relative-packages
 import { MosqueDTO } from '../../../back-end/src/db/models/mosques';
 import './MosquesByArea.css';
+// import { ReactComponent as DropdownIcon } from './dropdown-icon.svg';
 
 interface Props {
   area: string;
@@ -17,7 +18,11 @@ const AreaDropdown = ({ area, mosques }: Props) => {
   ));
   return (
     <div className="area-container px-3 py-2 min-w-[330px]">
-      <label htmlFor={`${area}-area-toggle`} className="area-toggle-label text-3xl font-medium cursor-pointer">
+      <label htmlFor={`${area}-area-toggle`} className="area-toggle-label text-3xl font-medium cursor-pointer flex flex-row">
+        <svg className="dropdown-icon-svg" viewBox="0 0 100 100" height="35px" stroke="black">
+          <line x1="25" y1="25" x2="75" y2="75" strokeWidth="7" />
+          <line x1="25" y1="75" x2="75" y2="25" strokeWidth="7" />
+        </svg>
         {area}
       </label>
       <input type="checkbox" id={`${area}-area-toggle`} className="area-toggle" />
