3b64ab987331f91cf097255cb22d278b936761bb
Nasir <nasir-islam@hotmail.com>
2023-07-10 20:52:28 +0100

    remove useLocationBtn and replace with link to FindNearestMosquePage

--- next-front-end/src/components/ControlPanel.tsx
+++ next-front-end/src/components/ControlPanel.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 import React from 'react';
 import { FiEye, FiEyeOff } from 'react-icons/fi';
 import SearchBar from './SearchBar';
-import UseLocationBtn from './UseLocationBtn';
+// import UseLocationBtn from './UseLocationBtn';
 // eslint-disable-next-line import/no-relative-packages
 import { MosqueDTO } from '../../../back-end/src/db/models/mosques';
 
@@ -19,7 +19,10 @@ const ControlPanel = ({ mosques, isMapVisible, setIsMapVisible }: Props) => (
     <SearchBar mosques={mosques} />
     <div className="bottom-options flex w-full max-w-xl justify-between py-1 px-2">
       {/* TODO: Replace UseLocationBtn with arrow and findNearestMosque Link */}
-      <UseLocationBtn />
+      <Link href="/findnearestmosque" className="font-semibold text-accent-700 hover:text-accent-800">
+        Find Nearest Mosque
+      </Link>
+      {/* <UseLocationBtn /> */}
       <button
         type="button"
         onClick={() => setIsMapVisible(!isMapVisible)}
@@ -36,7 +39,6 @@ const ControlPanel = ({ mosques, isMapVisible, setIsMapVisible }: Props) => (
           </>
         )}
       </button>
-      <Link href="/findnearestmosque">Find Nearest Mosque</Link>
     </div>
   </div>
 );
