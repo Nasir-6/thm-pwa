0bc09032d6ceed359375e609b65d207cb8b3287f
Nasir <nasir-islam@hotmail.com>
2023-07-15 12:23:37 +0100

    add react-query provider

--- next-front-end/src/app/layout.tsx
+++ next-front-end/src/app/layout.tsx
@@ -3,6 +3,7 @@ import './globals.css';
 import { Inter } from 'next/font/google';
 import Footer from '../components/Footer';
 import Navbar from '../components/Navbar';
+import Providers from '@/lib/providers';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -28,7 +29,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         {/* https://stackoverflow.com/questions/14270084/overflow-xhidden-doesnt-prevent-content-from-overflowing-in-mobile-browsers */}
         <div className="overflow-x-hidden w-screen relative">
           <Navbar />
-          {children}
+          <Providers>{children}</Providers>
           <Footer />
         </div>
       </body>
