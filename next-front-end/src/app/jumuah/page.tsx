269e2bc591ed8474cee5110752784d0defe4e8b8
Nasir <nasir-islam@hotmail.com>
2023-09-15 20:49:54 +0100

    add TODOS

--- next-front-end/src/app/jumuah/page.tsx
+++ next-front-end/src/app/jumuah/page.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 
-const page = () => {
+const JumuahPage = () => {
+  // TODO: Read in all jumuah times and display them.
+  // TODO: Have the ability to update rows in DB when changed all from this one page
   console.log('This is Jumuah page');
   return <div>Jumu'ah page</div>;
 };
 
-export default page;
+export default JumuahPage;
