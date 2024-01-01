faec3ab29ce49d75c2bc0de514b527894a56c1d4
Nasir <nasir-islam@hotmail.com>
2024-01-01 22:30:40 +0000

    style jumuah table

---
+++ next-front-end/src/app/jumuah/JumuahByArea.tsx
@@ -0,0 +1,29 @@
+import './JumuahByArea.css';
+import { MosqueJumuahTimes } from '../../../../back-end/src/db/models/jumuahTimes';
+
+interface Props {
+  area: string;
+  mosques: MosqueJumuahTimes[];
+}
+const JumuahByArea = ({ area, mosques }: Props) => {
+  const mosqueJumuahTimes = mosques.map((mosque, i) => (
+    <tr className={`bg-slate-${i % 2 ? '200' : '100'}`}>
+      <td>{mosque.mosqueName}</td>
+      <td>{mosque.firstTime}</td>
+      <td>{mosque.secondTime ? mosque.secondTime : '-'}</td>
+    </tr>
+  ));
+  return (
+    <>
+      <thead className="bg-primary-500 text-white text-left">
+        <tr>
+          <th>{area}</th>
+          <th>1st</th>
+          <th>2nd</th>
+        </tr>
+      </thead>
+      <tbody className="text-left">{mosqueJumuahTimes}</tbody>
+    </>
+  );
+};
+export default JumuahByArea;
--- next-front-end/src/app/jumuah/page.tsx
+++ next-front-end/src/app/jumuah/page.tsx
@@ -1,16 +1,29 @@
 import React from 'react';
 import { getAllJumuahTimes } from '@/lib/mosques';
+import { MosqueJumuahTimes } from '../../../../back-end/src/db/models/jumuahTimes';
+import JumuahByArea from './JumuahByArea';
 
 const JumuahPage = async () => {
   // TODO: Read in all jumuah times and display them.
   // TODO: Have the ability to update rows in DB when changed all from this one page
   const jumuahTimes = await getAllJumuahTimes();
-  const jumuahMosques = jumuahTimes?.map((mosque) => <p>{mosque.mosqueName}</p>);
-  console.log(jumuahTimes);
+  const areas: { [area: string]: MosqueJumuahTimes[] } = {};
+  jumuahTimes?.forEach((mosque) => {
+    if (mosque.area in areas) {
+      areas[mosque.area].push(mosque);
+    } else {
+      areas[mosque.area] = [mosque];
+    }
+  });
+
+  const jumuahByAreas = Object.keys(areas)
+    .sort()
+    .map((area) => <JumuahByArea area={area} mosques={areas[area]} />);
+
   return (
-    <div className="text-center">
+    <div className="mx-auto text-center">
       <h1>Jumu'ah page</h1>
-      {jumuahMosques}
+      <table className="jumuah-timetable m-4 shadow-sm overflow-hidden rounded">{jumuahByAreas}</table>
     </div>
   );
 };
