b900eaf070aecf14e471259a2ae71853a648d030
Nasir <nasir-islam@hotmail.com>
2024-01-01 22:34:01 +0000

    remove seconds for jumuah times

--- next-front-end/src/app/jumuah/JumuahByArea.tsx
+++ next-front-end/src/app/jumuah/JumuahByArea.tsx
@@ -9,8 +9,8 @@ const JumuahByArea = ({ area, mosques }: Props) => {
   const mosqueJumuahTimes = mosques.map((mosque, i) => (
     <tr className={`bg-slate-${i % 2 ? '200' : '100'}`}>
       <td>{mosque.mosqueName}</td>
-      <td>{mosque.firstTime}</td>
-      <td>{mosque.secondTime ? mosque.secondTime : '-'}</td>
+      <td>{mosque.firstTime.slice(0, 5)}</td>
+      <td>{mosque.secondTime ? mosque.secondTime.slice(0, 5) : '-'}</td>
     </tr>
   ));
   return (
